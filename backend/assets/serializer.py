1012011a26cf26ded093feeba4dc2f7cee59ddf2
jsonwamuyu <jysonmuchiri@gmail.com>
2025-06-13 07:26:34 +0300

    Setup homepage

---
+++ backend/assets/serializer.py
@@ -0,0 +1,2 @@
+class AssetSerializer:
+    pass
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 
+def homepage(request):
+    # return render(request, "homepage.html")
+    return HttpResponse(request, "Homepage")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("",homepage,name="homepage"),
     path("assets/", include("assets.urls", namespace="assets"))
 ]
